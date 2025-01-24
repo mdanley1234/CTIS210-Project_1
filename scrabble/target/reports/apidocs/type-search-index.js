440a6a2701da41b047efdad27ac5f8e07b794eb0
Michael Danley <112212314+mdanley1234@users.noreply.github.com>
2025-01-24 14:07:45 -0500

    javadoc

---
+++ scrabble/target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.guilford","l":"Main"},{"p":"edu.guilford","l":"ScrabbleSet"},{"p":"edu.guilford","l":"Tile"},{"p":"edu.guilford","l":"TileSet"}];updateSearchResults();
